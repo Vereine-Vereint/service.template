9944623fbbfea4854eb1fba5df88aae377d3343e
Johannes Schmid <Johannes Schmid>
2024-03-24 21:00:10 +0100

    updated service template

--- service.sh
+++ service.sh
@@ -14,26 +14,24 @@ source ./core/core.sh
 source ./borg/borg.sh
 
 # COMMANDS
-commands+=([example]="<msg>:Example command that prints <msg>")
-cmd_example() {
-  echo "Example: $1"
-}
+
+# This is an example command that prints a message from the first argument
+# commands+=([example]="<msg>:Example command that prints <msg>")
+# cmd_example() {
+#   echo "Example: $1"
+# }
 
 # ATTACHMENTS
-att_preStart() {
-  # execute before starting the service
-  echo "Pre Start"
-}
 
-att_configure() {
-  # execute before starting or restarting the service
-  updateTemplates
-}
+# Setup function that is called before the docker up command
+# att_setup() {
+#   echo "Setting up..."
+# }
 
-# FUNCTIONS
-updateTemplates() {
-  #
-  generate "example"
-}
+# Configure function that is called before the docker up, start and restart commands
+# att_configure() {
+#   echo "Configuring..."
+# }
 
+# MAIN
 main "$@"
